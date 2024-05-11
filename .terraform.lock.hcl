37348cbc4ad0b50dbdd86fcca5a60e66aa09d570
Mohamed <Mohamed.dine95@gmail.com>
2024-05-11 18:19:05 +0000

    #42 finish implementation of TF provider.

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:+T6hVhJitcznbC0gbn30FNDxnZthRR1CZvxJWtpBg1Q=",
+    "h1:Lc8n8KVJDM8zSxlfgTqkifOl4qElt9e0B6aFdi40X1g=",
   ]
 }
--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ terraform  {
 }
 
 provider "terratowns" {
-  endpoint = "lhttp://ocalhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid= "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token= "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -31,3 +31,25 @@ provider "terratowns" {
 #  index_html_filepath = var.index_html_filepath
 #  content_version = var.content_version
 #}
+
+#resource "terratowns_home" "home" {
+#  name = "Why Castlevania is the best animated show"
+#  description = "Castlevania is an animated show that aired on Netflix from 2017 to 2021. It is a show about a Vampire hunter who has a main goal of eliminating Dracula and his army."
+#    #domain_name = module.terrahouse_aws.cloudfront_url
+#    domain_name = "4rhfgry6.cloudfront.net"
+#    town = "gamers-grotto"
+#    content_version = 1
+  
+#}
+
+resource "terratowns_home" "home" {
+  name = "Why Castlevania is the best animated show!"
+  description = <<DESCRIPTION
+Castlevania is an animated show that aired on Netflix from 2017 to 2021. 
+It is a show about a Vampire hunter who has a main goal of eliminating Dracula and his army.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "4rhfgry6.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
