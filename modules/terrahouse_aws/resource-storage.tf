7670f4b130f14fa5ce912dab44f02d463246ebed
Mohamed <86280444+MoeDini95@users.noreply.github.com>
2024-05-18 16:40:57 -0400

    #48 test our terra home on Terratowns test town (#49)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -46,7 +46,7 @@ resource "aws_s3_object" "upload_assets" {
   key    = "assets/${each.key}"
   source = "${var.assets_path}/${each.key}"
   #content_type = "text/html"
-  etag = filemd5("${var.assets_path}${each.key}")
+  etag = filemd5("${var.assets_path}/${each.key}")
 
     lifecycle {
       replace_triggered_by = [terraform_data.content_version.output]
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
-#output "bucket_name" {
-#  description = "Bucket name for our static website hosting"
-#  value = module.terrahouse_aws
+output "bucket_name" {
+  description = "Bucket name for our static website hosting"
+  value = module.terrahouse_aws
 
-#}
+}
 
-#output "s3_website_endpoint" {
-#  description = "s3·static·website·hosting·endpoint"
-#  value = module.terrahouse_aws.website_endpoint
-#
+output "s3_website_endpoint" {
+  description = "s3·static·website·hosting·endpoint"
+  value = module.terrahouse_aws.website_endpoint
 
-#output "cloudfront_url" {
-#  description = "This CloudFront Distribution Domain Name"
-#  value = module.terrahouse_aws.cloudfront_url
-#}
+}
+
+output "cloudfront_url" {
+  description = "This CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
